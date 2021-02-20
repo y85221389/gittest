1ca035decb0bc21dfbd5de08511267e87d9c31fb
HYL <305863459@qq.com>
2021-02-20 09:48:24 +0800

    Initial Commit

---
+++ gittest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  gittest
+//
+//  Created by 一路惠 on 2021/2/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ gittestTests/gittestTests.m
@@ -0,0 +1,36 @@
+//
+//  gittestTests.m
+//  gittestTests
+//
+//  Created by 一路惠 on 2021/2/20.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gittestTests : XCTestCase
+
+@end
+
+@implementation gittestTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
